8390f7dec3993184fce3648b7efd2972004e6ac0
Jemeny123 <65108514+Jemeny123@users.noreply.github.com>
2020-09-27 17:19:32 +0800

    第一次上传

---
+++ 03react/appReact/src/componnets/header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Headcomponent extends React.Component {
+  render() {
+    return <div>测试-头部</div>;
+  }
+}
+
+export default Headcomponent;
---
+++ 03react/appReact/src/index.js
@@ -0,0 +1,23 @@
+// //在index.js写入react代码：
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// // import Headcomponent from "./componnets/header";
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         Hello React!
+//         {/* <Headcomponent /> */}
+//       </div>
+//     );
+//   }
+// }
+// export default App;
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+function show(){
+  console.log('打包成功');
+}
+show()
